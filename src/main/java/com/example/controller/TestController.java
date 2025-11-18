4eb44ffdf271b9cb8305ca36d4be552723db45d6
Sosigut <sosigut@gmail.com>
2025-11-18 22:31:08 +0300

    Add corss

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/ping")
     public String ping() {
         return "pong";
